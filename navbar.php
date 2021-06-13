53bee92f0ba6c610d56de33ae64f8ef1ea87d7ec
harika1210 <73464296+harika1210@users.noreply.github.com>
2021-06-13 21:42:08 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #662200;"><b><i>MKH BANK</i></b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #662200;"><b><i>Home</i></b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #662200;"><b><i>Transfer Money</i></b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #662200;"><b><i>Transaction History</i></b></a>
+              </li>
+          </div>
+       </nav>
